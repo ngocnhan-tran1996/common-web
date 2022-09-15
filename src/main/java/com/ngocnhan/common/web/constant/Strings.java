8169edfeed1da8449d309c50aeb812efd3334f97
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2022-09-16 00:11:51 +0700

    act: add constant string

---
+++ src/main/java/com/ngocnhan/common/web/constant/Strings.java
@@ -0,0 +1,12 @@
+package com.ngocnhan.common.web.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Strings {
+
+  public static final String EMPTY = "";
+  public static final String SPACE = " ";
+
+}
