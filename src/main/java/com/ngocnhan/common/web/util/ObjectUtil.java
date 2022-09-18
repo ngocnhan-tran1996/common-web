b6fbfd4f250b600be37077a8def591dd63ef5702
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2022-09-19 00:45:14 +0700

    act: add toString method

--- src/main/java/com/ngocnhan/common/web/util/ObjectUtil.java
+++ src/main/java/com/ngocnhan/common/web/util/ObjectUtil.java
@@ -1,6 +1,10 @@
 package com.ngocnhan.common.web.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 import lombok.AccessLevel;
@@ -8,6 +12,8 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ObjectUtil {
+
+  private static final ObjectMapper objectMapper = new ObjectMapper();
 
   public static <T> T getIfNull(T object, T defaultObject) {
     return object == null
@@ -25,4 +31,22 @@ public final class ObjectUtil {
         .orElse(null);
   }
 
+  public static String toString(Object obj) {
+
+    if (obj == null) {
+      return null;
+    }
+
+    try {
+      if (obj instanceof String) {
+        Map<String, Object> map = objectMapper.readValue((String) obj, new TypeReference<>() {
+        });
+        return objectMapper.writeValueAsString(map);
+      }
+
+      return objectMapper.writeValueAsString(obj);
+    } catch (JsonProcessingException e) {
+      return Objects.toString(obj);
+    }
+  }
 }
