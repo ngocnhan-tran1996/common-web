9b97230d1693756970592c3b485213cbbf7628ee
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2022-09-19 00:54:08 +0700

    act: add request log

---
+++ src/main/java/com/ngocnhan/common/web/util/LoggingUtil.java
@@ -0,0 +1,32 @@
+package com.ngocnhan.common.web.util;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class LoggingUtil {
+
+  public static void logRequest(HttpServletRequest httpServletRequest) {
+
+    var logRequest = new StringBuilder()
+        .append("REQUEST ")
+        .append("method=[").append(httpServletRequest.getMethod()).append("] ")
+        .append("path=[").append(httpServletRequest.getRequestURI()).append("] ")
+        .toString();
+
+    log.info(logRequest);
+  }
+
+  public static void logRequestBody(Object body) {
+
+    String requestBody = new StringBuilder()
+        .append("REQUEST BODY=")
+        .append(ObjectUtil.toString(body))
+        .toString();
+
+    log.info(requestBody);
+  }
+}
