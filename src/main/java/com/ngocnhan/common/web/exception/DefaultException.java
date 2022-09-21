7332aad3f021767d1f207e71729adad07b779eb5
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2022-09-21 23:28:19 +0700

    act: add serialize

--- src/main/java/com/ngocnhan/common/web/exception/DefaultException.java
+++ src/main/java/com/ngocnhan/common/web/exception/DefaultException.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 @Getter
 public class DefaultException extends RuntimeException {
 
+  private static final long serialVersionUID = 1L;
+
   private final ExceptionAttribute exceptionAttribute;
 
   public DefaultException(String code, String message, Integer statusCode) {
